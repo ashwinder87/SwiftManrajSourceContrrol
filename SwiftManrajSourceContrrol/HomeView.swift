f80f9e7624e3dd72706786a1ae489be3d0015865
Ashwinder <ashwinder87@gmail.com>
2025-05-26 17:25:37 +0530

    HomeView Updates(Progress)

--- SwiftManrajSourceContrrol/HomeView.swift
+++ SwiftManrajSourceContrrol/HomeView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Screen Two")
+        ZStack {
+            Text("screen 1")
+            
+            Text("screen 2")
+            Text("screen 3")
+        }
     }
 }
 
