5e0600d7a7b98aa7489098d4f8e15de660151228
Ashwinder <ashwinder87@gmail.com>
2025-05-26 17:30:31 +0530

    change click me background color ,  change text to manraj , something else...

--- SwiftManrajSourceContrrol/ContentView.swift
+++ SwiftManrajSourceContrrol/ContentView.swift
@@ -13,12 +13,12 @@ struct ContentView: View {
             Image(systemName: "heart.fill")
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
-            Text("Hello, Manraj Thinking!")
+            Text("Manraj Thinking!")
             
             Button("Click Me") {
                 
             }
-            .background(Color.red)
+            .background(Color.green)
         }
         .padding()
     }
