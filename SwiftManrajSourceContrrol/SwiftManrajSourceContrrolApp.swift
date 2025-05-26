e22bfdd5080b4129a8b2084a4a9ba2040c0bf611
Ashwinder <ashwinder87@gmail.com>
2025-05-26 17:02:50 +0530

    Initial Commit

---
+++ SwiftManrajSourceContrrol/SwiftManrajSourceContrrolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftManrajSourceContrrolApp.swift
+//  SwiftManrajSourceContrrol
+//
+//  Created by ASHWINDER SINGH on 26/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftManrajSourceContrrolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
