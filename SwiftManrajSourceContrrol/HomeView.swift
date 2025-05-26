af69adc60a084c92e5c4c9f46eed16aa4f26df08
Ashwinder <ashwinder87@gmail.com>
2025-05-26 17:24:36 +0530

    Add HomeView

---
+++ SwiftManrajSourceContrrol/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftManrajSourceContrrol
+//
+//  Created by ASHWINDER SINGH on 26/05/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen Two")
+    }
+}
+
+#Preview {
+    HomeView()
+}
