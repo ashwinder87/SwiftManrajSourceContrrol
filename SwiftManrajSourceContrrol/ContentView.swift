ddd64d3f5504e347acd65effda864f437493f960
Ashwinder <ashwinder87@gmail.com>
2025-05-27 16:50:00 +0530

    [Feature] Subscribe New added back

--- SwiftManrajSourceContrrol/ContentView.swift
+++ SwiftManrajSourceContrrol/ContentView.swift
@@ -46,7 +46,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Manraj Thinking !")
             
-            Button("Subscribe") {
+            Button("Subscribe now!") {
                 
             }
            Rectangle()
